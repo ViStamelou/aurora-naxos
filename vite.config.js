43a01f647c3ef8e206e19fe6abf61436abf08c1b
ViStamelou <vasilikist.97@gmail.com>
2025-03-19 14:18:41 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/aurora-naxos/' // Absolute path, necessary for GitHub Pages subdirectory
+  plugins: [react()]
 })
